9a901772459cd238cd29d631bc372e016488ba00
philiusjd@gmail.com <philiusjd@gmail.com>
2017-08-01 16:48:52 -0400

    added dbContext for CalendarEvent, added connection string in webConfig to use db for CalendarEvents.

---
+++ GoogCalWebApp/GoogCalWebApp/Models/CalendarEventContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace GoogCalWebApp.Models
+{
+    public class CalendarEventContext : DbContext
+    {
+        public DbSet<Event> Events { get; set; }
+    }
+}
