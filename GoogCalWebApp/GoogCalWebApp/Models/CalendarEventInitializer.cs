d4e3535ac6001aede043aa745b3999964aded53e
philiusjd@gmail.com <philiusjd@gmail.com>
2017-08-01 17:02:14 -0400

    Create a initializer to seed the db with events and modified the Global.asax file.

---
+++ GoogCalWebApp/GoogCalWebApp/Models/CalendarEventInitializer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace GoogCalWebApp.Models
+{
+    public class CalendarEventInitializer: DropCreateDatabaseIfModelChanges<CalendarEventContext>
+    {
+        protected override void Seed(CalendarEventContext context)
+        {
+            var events = new List<Event>
+            {
+                new Event { Id = 2}
+            };
+
+
+            foreach(var temp in events)
+            {
+                context.Events.Add(temp);
+            }
+
+            context.SaveChanges();
+        }
+    }
+}
