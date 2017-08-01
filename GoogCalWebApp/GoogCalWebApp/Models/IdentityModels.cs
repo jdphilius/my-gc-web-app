8dbf28a6d3eb8b2f722ded2fc39aad6c9dc372f6
philiusjd@gmail.com <philiusjd@gmail.com>
2017-07-31 22:58:24 -0400

    added Event class

--- GoogCalWebApp/GoogCalWebApp/Models/IdentityModels.cs
+++ GoogCalWebApp/GoogCalWebApp/Models/IdentityModels.cs
@@ -20,6 +20,7 @@ namespace GoogCalWebApp.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Event> Events { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
